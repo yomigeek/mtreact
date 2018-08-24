8f6554f4e2348e161693ea4c9f2031e3500cb879
Oyomi <seayomi@gmail.com>
2018-08-24 14:48:54 +0100

    implement changes to package.json

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 let app = express();
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'client')));
 const port = process.env.PORT || '8080';
 app.set('port', port);
 const server = http.createServer(app);
